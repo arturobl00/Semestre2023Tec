8e44443e52cc0c826b6a166837d12a9b8bac7dfc
arturobl00 <arturobl00@msn.com>
2023-02-02 13:40:57 -0600

    Semana 1

---
+++ APP Hibridas/Semana 1/demo1/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: Demo1(),
+    );
+  }
+}
+
+class Demo1 extends StatefulWidget {
+  const Demo1({super.key});
+
+  @override
+  State<Demo1> createState() => _Demo1State();
+}
+
+class _Demo1State extends State<Demo1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mi APP"),
+      ),
+      body: Container(
+        child: Text(
+          "Hola",
+          style: TextStyle(color: Colors.blue),
+        ),
+      ),
+    );
+  }
+}
