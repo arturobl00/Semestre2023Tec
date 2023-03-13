85d9727c009a85fba767487b4891b015044b101a
arturobl00 <arturobl00@msn.com>
2023-03-13 13:06:06 -0600

    13031

--- APP Hibridas/Semana 6/pokeapi/lib/main.dart
+++ APP Hibridas/Semana 6/pokeapi/lib/main.dart
@@ -40,17 +40,55 @@ class MyPokemon extends StatelessWidget {
               itemCount: pokemons.length,
               itemBuilder: (context, index) {
                 final pokemon = pokemons[index];
-                return Card(
+                return Container(
+                  margin: const EdgeInsets.only(
+                    left: 20.0,
+                    top: 10.0,
+                    bottom: 50.0,
+                  ),
+                  width: 30.0,
                   child: Column(
                     children: [
-                      Text("Nombre: ${pokemon.name}"),
-                      Text("Abilidad: ${pokemon.abilities}"),
-                      Text("Movimiento: ${pokemon.moves}"),
-                      Text("Tipo: ${pokemon.type}"),
-                      Image.network(pokemon.imageUrl)
+                      Container(
+                        height: 300,
+                        width: 200,
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("assets/images.png"),
+                                fit: BoxFit.cover)),
+                        child: Image.network(
+                          pokemon.imageUrl,
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10.0),
+                        decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                            )),
+                        child: Row(
+                          children: [
+                            Column(
+                              children: [
+                                Text("Nombre: ${pokemon.name}"),
+                                Text("Tipo: ${pokemon.type}"),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text("Habilidad: ${pokemon.abilities}"),
+                                Text("Movimiento: ${pokemon.moves}"),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
                     ],
                   ),
                 );
+
                 /*ListTile(
                   leading: Image.network(pokemon.imageUrl),
                   title: Text(pokemon.name),
@@ -95,7 +133,7 @@ class PokemonApi {
 
   //Declarar la lista de elementos que tiene el api
   static Future<List<Pokemon>> fetchPokemons() async {
-    final response = await http.get(Uri.parse('$_baseUrl/pokemon?limit=15'));
+    final response = await http.get(Uri.parse('$_baseUrl/pokemon?limit=3'));
     if (response.statusCode == 200) {
       print(response.body);
       final decodedBody = jsonDecode(response.body);
@@ -120,3 +158,10 @@ class PokemonApi {
     }
   }
 }
+
+
+/*Text("Nombre: ${pokemon.name}"),
+ Text("Abilidad: ${pokemon.abilities}"),
+ Text("Movimiento: ${pokemon.moves}"),
+ Text("Tipo: ${pokemon.type}"),
+ Image.network(pokemon.imageUrl)*/
---
+++ Topicos Av de Programación/Semana 7/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Semana 7 Unidad 3 Programación Concurrente</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="display-3 text-center">Semana 7 Ejemplo 1 - Programación Concurrente</h1>
+        <h2 class="display-6 text-center">Ejecución de Ciclo Para mostrar un mensaje en varios estilos</h2>
+        <form method="post">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="mensaje" 
+                id="floatingInput" placeholder="Coloque su Mensaje Aqui"/>
+                <label for="floatingInput">Coloque su Mensaje Aqui</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="number" class="form-control" name="ciclos" 
+                id="floatingInput" placeholder="Coloque un número"/>
+                <label for="floatingInput">Coloque un número</label>
+            </div>
+            <div>
+                <input type="submit" class="btn btn-primary" name="enviar" value="Enviar"/>
+            </div>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</body>
+</html>
