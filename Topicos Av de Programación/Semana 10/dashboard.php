a86cb2ba22a700cc53996c14c3a7774970f879a4
arturobl00 <arturobl00@msn.com>
2023-04-19 15:20:10 -0600

    19/04a

---
+++ Topicos Av de Programación/Semana 10/dashboard.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Sistema de Punto de Venta</title>
+</head>
+<body class="container">
+    <div class="container text-center mt-5">
+        <div class="row">
+            <div class="col">
+            <img alt="" src="assets/tpv.png" width="70"/>
+            </div>
+            <div class="col">
+                <h1>Sistema Terminal de Punto de Venta</h1>
+            </div>
+            <div class="col">
+            <a href="#"><img alt="informacion" src="assets/informacion.png" width="70"/></a>
+            <a href="#"><img alt="cerrar sesion" src="assets/verificar.png" width="70"/></a>
+            </div>
+        </div>
+        <div class="row text-bg-primary p-1 m-1">
+            <div class="col">
+            <p>Nombre Usuario: admin</p>
+            </div>
+            <div class="col">
+            <p>Fecha / Hora de Ingreso: 17/04/2023 03:27:12</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <img alt="" src="assets/ventas.png" width="100"/>
+                <p>Ventas</p>
+            </div>
+            <div class="col">
+            <img alt="" src="assets/compras.png" width="100"/>
+                <p>Compras</p>
+            </div>
+            <div class="col">
+            <img alt="" src="assets/productos.png" width="100"/>
+                <p>Productos</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+            <img alt="" src="assets/clientes.png" width="100"/>
+                <p>Clientes</p>
+            </div>
+            <div class="col">
+            <img alt="" src="assets/proveedor.png" width="100"/>
+                <p>Proveedores</p>
+            </div>
+            <div class="col">
+            <a href="./modulos/mantenimiento.php"><img alt="" src="assets/mantenimiento.png" width="70"/></a>
+                <p>Mantenimiento</p>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ Topicos Av de Programación/Semana 10/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Sistema de Punto de Venta</title>
+</head>
+<body class="container">
+    <div class="container mt-5 border">
+        <div class="row mt-3 mb-3">
+            <div class="col-5">
+                <img src="assets/bannerleft.jpg" alt="" width="100%" height="700"/>
+            </div>
+            <div class="col-7">
+                <form>
+                    <h2>Sign in to Shope!</h2>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">User Name</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <div id="emailHelp" class="form-text">We'll never share your username with anyone else.</div>
+                    </div>
+                    
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1"/>
+                    </div>
+    
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+</body>
+</html>
